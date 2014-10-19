fdddb8b238dcd827c0b14d8642ffc23bd3c55e45
jackscotti <jacopojackscotti@gmail.com>
2014-10-19 13:04:55 +0100

    Add splitBySpaces method

--- Coursework2/src/FractionCalculator.java
+++ Coursework2/src/FractionCalculator.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class FractionCalculator {
+	static Scanner input = new Scanner(System.in);
+	final static String DELIMITER = " ";
+	static String[] splittedString;
+	
+	public static void main(String[] args) {
+			
+		String input = getInput();
+		FractionCalculator calc = new FractionCalculator();
+		calc.splitBySpaces(input);
+	}
+	
+	public static String getInput(){
+		String i;
+		System.out.println("Type in:");
+		i = input.nextLine();
+		return i;
+	}
 
+	public String[] splitBySpaces(String string) {
+		splittedString = string.split(DELIMITER);
+		return splittedString;
+	}
 }
