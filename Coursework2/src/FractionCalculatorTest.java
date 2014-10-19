b386d61bd1c32b6db65c823bbbbb068105e1c892
jackscotti <jacopojackscotti@gmail.com>
2014-10-19 12:52:15 +0100

    Add splitBySpaces method tests

--- Coursework2/src/FractionCalculatorTest.java
+++ Coursework2/src/FractionCalculatorTest.java
@@ -1,13 +1,13 @@
-//import static org.junit.Assert.*;
-//
-//import org.junit.Test;
-//
-//
-//public class FractionCalculatorTest {
-//
-//	@Test
-//	public void test() {
-//		fail("Not yet implemented");
-//	}
-//
-//}
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FractionCalculatorTest {
+    
+    @Test
+	public void splitBySpaces() {
+    	FractionCalculator calc = new FractionCalculator();
+    	String[] testArray = calc.splitBySpaces("Jacopo Scotti SP2 Coursework2");
+    	String[] checkArray = {"Jacopo", "Scotti", "SP2", "Coursework2"};
+    	assertArrayEquals(checkArray, testArray);
+	}
+}
