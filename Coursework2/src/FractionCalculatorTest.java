62f45df63dbd825f7a909dc603a9f8b67fc19d50
jackscotti <jacopojackscotti@gmail.com>
2014-10-19 14:01:14 +0100

    Add toNumber test

--- Coursework2/src/FractionCalculatorTest.java
+++ Coursework2/src/FractionCalculatorTest.java
@@ -2,20 +2,28 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class FractionCalculatorTest {
+	
+	FractionCalculator calc = new FractionCalculator();
     
     @Test
 	public void splitBySpaces() {
-    	FractionCalculator calc = new FractionCalculator();
     	String[] testArray = calc.splitBySpaces("Jacopo Scotti SP2 Coursework2");
     	String[] checkArray = {"Jacopo", "Scotti", "SP2", "Coursework2"};
     	assertArrayEquals(checkArray, testArray);
 	}
     
+    @Test
     public void splitBySlash() {
-    	FractionCalculator calc = new FractionCalculator();
     	String[] testArray = calc.splitBySlash("1/3");
     	String[] checkArray = {"1", "3"};
     	assertArrayEquals(checkArray, testArray);
 	}
+    
+    @Test
+    public void toNumber(){
+    	int[] testArray = calc.toNumber("1", "3");
+    	int[] checkArray = {1, 3};
+    	assertArrayEquals(checkArray, testArray);
+    }
 
 }
