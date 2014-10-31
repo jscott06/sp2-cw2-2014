03508e4a4b6220e49c7d70c0ff7c863610b58d58
jackscotti <jacopojackscotti@gmail.com>
2014-10-31 19:21:58 +0000

    Refactoring of readAndCalculate - internal operations replaced by other methods - no tests needed because readAndCalculate method was tested already - renaming variables - add comments

--- Coursework2/src/FractionCalculator.java
+++ Coursework2/src/FractionCalculator.java
@@ -10,17 +10,21 @@ public class FractionCalculator {
 	
 	public static void main(String[] args) {
 		
-		String input = getInput();
-		splittedString = split(input, " ");
+		// add here do-while loop for keeping asking input using new operator values "continue" and "Q"
 		FractionCalculator calc = new FractionCalculator();
-		calc.readAndCalculate(splittedString);
-		System.out.println(calc.getMemory());
-		System.out.println("END");
+		System.out.println("Memory: " + calc.getMemory());
+		String input = getInput();
+		
+		splittedString = split(input, " ");
+		calc.readAndCalculate(splittedString);		
+		System.out.println("Final result: " + calc.getMemory());
+
 		
 		// TODO
 		// - Allow multi line input with loop and break character
-		// - Print total everytime memory is being 'touched'
 		// - Wrap everything (Input, Read input, Split input, Calculate, Return Total and ask input in a single method
+		// - Create a printing method that returns what to print (Testable)
+		// - if there is already an operator saved on memory raise exception and quit (ie 1 + + 1 not allowed)
 		
 	}
 	
@@ -110,36 +114,48 @@ public class FractionCalculator {
 	public Fraction clear(){
 		return new Fraction(0);
 	}
+	
+	public Fraction inputToFraction(String stringFraction){
+		String[] stringNumAndDen = split(stringFraction, "/");
+    	int[] numAndDen = toNumber(stringNumAndDen);
+    	Fraction f = toFraction(numAndDen); // refactor
+		return f;
+	}
+	
+	public void initialiseOrOperateOnMemory(String input){
+		if (getOperator() == "not initialised") {
+        	setMemory(inputToFraction(input));
+        	setOperator("");
+    	} else {
+        	calculate(getMemory(), getOperator(), inputToFraction(input));
+    	}
+	}
 
-	public void readAndCalculate(String[] input) {
-		// add here do-while loop for keeping asking input using new operator values "continue" and "Q"
-		for (String i : input) {
-			if (i.matches("[-*/+]{1}")){
+	public void readAndCalculate(String[] inputLine) {
+		for (String element : inputLine) {
+			
+			if (element.matches("[-*/+]{1}")){
+				// I learnt how to use regular expressions here: http://www.regexr.com/
 				// all the operations
-				setOperator(i);
-			} else if (i.matches("((neg)|(N)|(abs)|(a)|(A)|(clear)|(C)|(c)){1}")) {
+				setOperator(element);
+			} else if (element.matches("((neg)|(N)|(abs)|(a)|(A)|(clear)|(C)|(c)){1}")) {
 				// all the functions
-				operateOnMemory(i);
-			} else if (i.matches("(-{0,1}[0-9]+\\/{1}-{0,1}[1-9]+)|(-{0,1}[0-9]+)")) {
+				operateOnMemory(element);
+			} else if (element.matches("(-{0,1}[0-9]+\\/{1}-{0,1}[1-9]+)|(-{0,1}[0-9]+)")) {
 				// every fraction negative or positive with denominator!= 0 + whole numbers
-				String[] stringNumAndDen = split(i, "/");
-	        	int[] numAndDen = toNumber(stringNumAndDen);
-	        	Fraction f = toFraction(numAndDen); // refactor
-	        	if (getOperator() == "not initialised") {
-	            	setMemory(f);
-	            	setOperator("");
-	        	} else {
-		        	calculate(getMemory(), getOperator(), f);
-	        	}
+	        	initialiseOrOperateOnMemory(element);
+			} else if (element.matches("((q)|(Q)|(quit)){1}")) {
+				// Raise an exception
+				System.out.println("You decided to quit"); 
+				// TODO this needs to quit the calculator and stop to ask user input
 			} else {
+				// Stop processing any remaining input, set the value in the calculator to zero,
+				// and raise an exception
+				setMemory(new Fraction(0));
 				System.out.println("Unexpected input");
 				break;
 			}
-//			System.out.print("Input");
-//			System.out.println(i);
-//			System.out.print("Memory:");
-//			System.out.println(getMemory());
+			System.out.println("Memory: " + getMemory());
 		}
 	}
-
 }
